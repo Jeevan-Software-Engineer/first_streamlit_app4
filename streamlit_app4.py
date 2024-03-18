30d8498f4bdbbc6aec00652c263a5861443440ac
Jeevan-Software-Engineer <160514292+Jeevan-Software-Engineer@users.noreply.github.com>
2024-03-18 12:21:53 +0530

    Update streamlit_app4.py

--- streamlit_app4.py
+++ streamlit_app4.py
@@ -4,7 +4,7 @@ import pandas
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
 # connect to snowflake
 # def init_connection():
-my_cnx = snowflake.connector.Connect(**streamlit.Secrets["snowflake"])
+my_cnx = snowflake.connector.Connect(https://firstappapp4-cjsshjqgew65stluachczr.streamlit.app/.Secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
